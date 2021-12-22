7abc4cb6adf32180b7a151ddac2b598458f050af
PaulineJac <p.jacsont@gmail.com>
2021-12-22 16:49:14 +0100

    model_htr_v4

--- model_htr_v4/submission-script.sh
+++ model_htr_v4/submission-script.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
-#SBATCH --partition=shared-gpu
-#SBATCH --time=03:00:00
+#SBATCH --partition=public-gpu
+#SBATCH --time=20:00:00
 #SBATCH --gpus=1
 #SBATCH --output=kraken-%j.out
 #SBATCH --mem=0
@@ -9,7 +9,7 @@ module load fosscuda/2020b Python/3.8.6
 source ~/Sandozenv/bin/activate
 
 OUTPUT_NAME="output_name"
-XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/GT_Alto/"
+XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Varios-GroundTruth/"
 
 echo "KETOS training"
-srun ketos train -o $OUTPUT_NAME -f alto -d cuda "${XML_FOLDER}/*.xml"
+srun ketos train -o $OUTPUT_NAME -f page -d cuda "${XML_FOLDER}/*.xml"

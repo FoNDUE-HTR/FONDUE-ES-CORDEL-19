157619104430b02c6a7f3fa668cc607655247e2c
PaulineJac <p.jacsont@gmail.com>
2021-12-23 09:01:40 +0100

    test_bin_Otsu

--- submission-script.sh
+++ submission-script.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
-#SBATCH --partition=shared-gpu
-#SBATCH --time=03:00:00
+#SBATCH --partition=public-gpu
+#SBATCH --time=20:00:00
 #SBATCH --gpus=1
 #SBATCH --output=kraken-%j.out
 #SBATCH --mem=0
@@ -8,8 +8,12 @@
 module load fosscuda/2020b Python/3.8.6
 source ~/Sandozenv/bin/activate
 
+work_directory="/home/users/j/jacsont/Varios-OCR-files/Varios-GT-Test/model_htr_test_bin"
+mkdir -p ${work_directory}
+cd ${work_directory}
+
 OUTPUT_NAME="output_name"
-XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/GT_Alto/"
+XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Varios-GT-Test/GroundTruth-binary-Otsu/"
 
 echo "KETOS training"
-srun ketos train -o $OUTPUT_NAME -f alto -d cuda "${XML_FOLDER}/*.xml"
+srun ketos train -o $OUTPUT_NAME -f page -d cuda "${XML_FOLDER}/*.xml"

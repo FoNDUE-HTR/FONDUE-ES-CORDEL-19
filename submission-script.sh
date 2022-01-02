05852226eca6ac27e8b864b5f42df17a69c113d4
PaulineJac <p.jacsont@gmail.com>
2022-01-02 08:51:26 +0100

    test-bin

--- submission-script.sh
+++ submission-script.sh
@@ -8,12 +8,12 @@
 module load fosscuda/2020b Python/3.8.6
 source ~/Sandozenv/bin/activate
 
-work_directory="/home/users/j/jacsont/Varios-OCR-files/Models-htr-Varios/Alto/Model-htr-test-OTSU-v2/"
+work_directory="/home/users/j/jacsont/Varios-OCR-files/Models-htr-Varios/Alto/Model-htr-test-binary/"
 mkdir -p ${work_directory}
 cd ${work_directory}
 
 OUTPUT_NAME="output_name"
-XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Varios-GroundTruth-Test-Alto/GroundTruth-binary-Otsu/"
+XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Varios-GroundTruth-Test-Alto/Varios-GroundTruth-binary/"
 
 echo "KETOS training"
 srun ketos train -t /home/users/j/jacsont/Varios-OCR-files/Split/train.txt -e /home/users/j/jacsont/Varios-OCR-files/Split/eval.txt -f alto -d cuda "${XML_FOLDER}/*.xml"

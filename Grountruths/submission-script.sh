9d8d31d7ba12c132f8df4ac8f20571b37412f58e
PaulineJac <p.jacsont@gmail.com>
2022-01-07 08:01:05 +0100

    Gamma2+Niblack

--- Grountruths/submission-script.sh
+++ Grountruths/submission-script.sh
@@ -8,12 +8,12 @@
 module load fosscuda/2020b Python/3.8.6
 source ~/Sandozenv/bin/activate
 
-work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-htr-Varios/Alto/Model-htr-test-gamma2/"
+work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-htr-Varios/Alto/Model-htr-test-gamma2+Niblack/"
 mkdir -p ${work_directory}
 cd ${work_directory}
 
 OUTPUT_NAME="output_name"
-XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Grountruths/Varios-GroundTruth-Test-Alto/GroundTruth-gamma2/"
+XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Grountruths/Varios-GroundTruth-Test-Alto/GroundTruth-Gamma2+Niblack/"
 
 echo "KETOS training"
 srun ketos train -t /home/users/j/jacsont/Varios-OCR-files/Grountruths/Split/train.txt -e /home/users/j/jacsont/Varios-OCR-files/Grountruths/Split/eval.txt -f alto -d cuda "${XML_FOLDER}/*.xml"

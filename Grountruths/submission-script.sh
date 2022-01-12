80b1be5b07a956fb5d31ce0bbfc06450b830c2b4
PaulineJac <p.jacsont@gmail.com>
2022-01-12 10:01:41 +0100

    train_seg

--- Grountruths/submission-script.sh
+++ Grountruths/submission-script.sh
@@ -8,12 +8,12 @@
 module load fosscuda/2020b Python/3.8.6
 source ~/Sandozenv/bin/activate
 
-work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-htr-Varios/Alto/Model-htr-test-gamma2+Otsu/"
+work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-segmentation/model_seg_1/"
 mkdir -p ${work_directory}
 cd ${work_directory}
 
 OUTPUT_NAME="output_name"
-XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Grountruths/Varios-GroundTruth-Test-Alto/GroundTruth-Gamma2+Otsu/"
+XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Grountruths/Varios-GrounTruth-Alto/"
 
 echo "KETOS training"
-srun ketos train -t /home/users/j/jacsont/Varios-OCR-files/Grountruths/Split/train.txt -e /home/users/j/jacsont/Varios-OCR-files/Grountruths/Split/eval.txt -f alto -d cuda "${XML_FOLDER}/*.xml"
+srun ketos segtrain -t /home/users/j/jacsont/Varios-OCR-files/Grountruths/Split/train.txt -e /home/users/j/jacsont/Varios-OCR-files/Grountruths/Split/eval.txt -f alto -d cuda "${XML_FOLDER}/*.xml"

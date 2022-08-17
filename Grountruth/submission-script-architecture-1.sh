d2a18c239785b65966290113f079d5134d3b41c1
PaulineJac <p.jacsont@gmail.com>
2022-08-17 15:33:48 +0200

    17.08

--- Grountruth/submission-script-architecture-1.sh
+++ Grountruth/submission-script-architecture-1.sh
@@ -9,15 +9,14 @@ module load fosscuda/2020b Python/3.8.6
 source ~/Sandozenv/bin/activate
 pip install albumentations
 
-work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-HTR-Varios/Alto/Model_test"
+work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-HTR-Varios/Alto/Model_data+varios_4"
 mkdir -p ${work_directory}
 cd ${work_directory}
 
-OUTPUT_NAME="output_name"
+OUTPUT_NAME="Data+varios_4"
 
 echo "KETOS training"
 
-ketos train -f alto  -i /home/users/j/jacsont/Varios-OCR-files/Models/Models-HTR-Varios/Model-htr-test-Niblack/model_best.mlmodel /home/users/j/jacsont/Varios-OCR-files/Grountruth/Data+varios/*.xml
-
+srun ketos train  -r 0.0001 --lag 20  -f alto  -s '[1,120,0,1 Cr3,13,32 Do0.1,2 Mp2,2 Cr3,13,32 Do0.1,2 Mp2,2 Cr3,9,64 Do0.1,2 Mp2,2 Cr3,9,64 Do0.1,2 S1(1x0)1,3 Lbx200 Do0.1,2 Lbx200 Do.1,2 Lbx200 Do]' --threads 20 -u NFD --augment -d cuda --preload "${XML_FOLDER}/*.xml"
 
 

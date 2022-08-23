95c2deaca24fbd4753edae0f9dee149e0590db4e
PaulineJac <p.jacsont@gmail.com>
2022-08-23 09:25:12 +0200

    23.08

--- Grountruth/submission-script-architecture-1.sh
+++ Grountruth/submission-script-architecture-1.sh
@@ -14,7 +14,7 @@ mkdir -p ${work_directory}
 cd ${work_directory}
 
 OUTPUT_NAME="Data+varios_4"
-
+XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Grountruth/Data+varios/"
 echo "KETOS training"
 
 srun ketos train  -r 0.0001 --lag 20  -f alto  -s '[1,120,0,1 Cr3,13,32 Do0.1,2 Mp2,2 Cr3,13,32 Do0.1,2 Mp2,2 Cr3,9,64 Do0.1,2 Mp2,2 Cr3,9,64 Do0.1,2 S1(1x0)1,3 Lbx200 Do0.1,2 Lbx200 Do.1,2 Lbx200 Do]' --threads 20 -u NFD --augment -d cuda --preload "${XML_FOLDER}/*.xml"

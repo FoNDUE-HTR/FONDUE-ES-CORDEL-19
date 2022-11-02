d9dd8168a540ceac6178cacd46b7d3ea36d005e9
PaulineJac <p.jacsont@gmail.com>
2022-11-02 12:38:20 +0100

    02.11

--- Grountruth/submission-script-architecture-1.sh
+++ Grountruth/submission-script-architecture-1.sh
@@ -6,8 +6,7 @@
 #SBATCH --mem=0
 
 module load fosscuda/2020b Python/3.8.6
-source ~/Sandozenv/bin/activate
-pip install albumentations
+source ~/test-env/bin/activate
 
 work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-HTR-Varios/Alto/Model_data+varios_4"
 mkdir -p ${work_directory}
@@ -17,6 +16,6 @@ OUTPUT_NAME="Data+varios_4"
 XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Grountruth/Data+varios/"
 echo "KETOS training"
 
-srun ketos train  -r 0.0001 --lag 20  -f alto  -s '[1,120,0,1 Cr3,13,32 Do0.1,2 Mp2,2 Cr3,13,32 Do0.1,2 Mp2,2 Cr3,9,64 Do0.1,2 Mp2,2 Cr3,9,64 Do0.1,2 S1(1x0)1,3 Lbx200 Do0.1,2 Lbx200 Do.1,2 Lbx200 Do]' --threads 20 -u NFD --augment -d cuda --preload "${XML_FOLDER}/*.xml"
+srun ketos train  -r 0.0001 --lag 20  -f alto  -s '[1,120,0,1 Cr3,13,32 Do0.1,2 Mp2,2 Cr3,13,32 Do0.1,2 Mp2,2 Cr3,9,64 Do0.1,2 Mp2,2 Cr3,9,64 Do0.1,2 S1(1x0)1,3 Lbx200 Do0.1,2 Lbx200 Do.1,2 Lbx200 Do]' --workers 20 -u NFD --augment "${XML_FOLDER}/*.xml"
 
 
--- Grountruth/submission-script.sh
+++ Grountruth/submission-script.sh
@@ -6,16 +6,15 @@
 #SBATCH --mem=0
 
 module load fosscuda/2020b Python/3.8.6
-source ~/Sandozenv/bin/activate
-pip install albumentations
+source ~/kraken-env/bin/activate
 
-work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-HTR-Varios/Model_BGR"
+
+work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-HTR-Varios/Model_data+varios_3"
 mkdir -p ${work_directory}
 cd ${work_directory}
 
 OUTPUT_NAME="output_name"
 
-XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Grountruth/Data/"
+XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Grountruth/Data+varios"
 echo "KETOS training"
-srun ketos train -f alto -t /home/users/j/jacsont/Varios-OCR-files/Grountruth/Split/train.txt -e /home/users/j/jacsont/Varios-OCR-files/Grountruth/Split/eval.txt -d cuda "${XML_FOLDER}/*.xml"
-ketos test -m /Users/Mac-Pauline/Documents/Sandoz/Varios-OCR-files/Models/Models-htr-Varios/Alto/Model-htr-test-Niblack/model_best.mlmodel -e /Users/Mac-Pauline/Documents/Sandoz/Varios-OCR-files/Grountruths/Split/test.txt -f alto
+srun ketos train -f alto  "${XML_FOLDER}/*.xml"

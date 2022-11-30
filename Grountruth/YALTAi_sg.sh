bd007bc840743d69cbd682af31efc747afc7ff8c
PaulineJac <p.jacsont@gmail.com>
2022-11-30 15:53:03 +0100

    Yaltai_6

--- Grountruth/YALTAi_sg.sh
+++ Grountruth/YALTAi_sg.sh
@@ -7,4 +7,5 @@
 module load fosscuda/2020b Python/3.8.6
 source ~/Kraken-env/bin/activate
 
-srun yaltai kraken --device cuda:0 -I "/home/users/j/jacsont/Varios-OCR-files/Grountruth/Segmentation_Yaltai/*.jpg" --suffix ".xml" segment --yolo /home/users/j/jacsont/Yaltai/yolov5/runs/train/exp2/weights/best.pt
+srun yaltai kraken --device cuda:0 -I "/home/users/j/jacsont/Varios-OCR-files/Grountruth/Segmentation_Yaltai/train/images/*.jpg" --suffix ".xml" segment --yolo /home/users/j/jacsont/Yaltai/yolov5/runs/train/exp2/weights/best.pt
+

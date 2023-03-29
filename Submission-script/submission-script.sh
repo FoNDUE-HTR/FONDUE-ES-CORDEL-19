02edc2b624b937fff5027ac01b0c2398c9578d3f
PaulineJac <p.jacsont@gmail.com>
2023-03-29 10:57:15 +0200

    29.03

--- Submission-script/submission-script.sh
+++ Submission-script/submission-script.sh
@@ -2,19 +2,21 @@
 #SBATCH --partition=public-gpu
 #SBATCH --time=2-00:00:00
 #SBATCH --gpus=1
-#SBATCH --output=kraken-%j.out
-#SBATCH --mem=0
+#SBATCH --output=kraken_HTRAll-%j.out
+#SBATCH --mem=12GB
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1,VramPerGpu:12GB
 
 module load fosscuda/2020b Python/3.8.6
 source ~/kraken-env/bin/activate
 
 
-work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-HTR-Varios/Model_data+varios_3"
+work_directory="/home/users/j/jacsont/Varios-OCR-files/Models/Models-HTR-Varios/HTR_All"
 mkdir -p ${work_directory}
 cd ${work_directory}
 
-OUTPUT_NAME="output_name"
+OUTPUT_NAME="HTRAll"
 
-XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Grountruth/Data+varios"
+XML_FOLDER="/home/users/j/jacsont/Varios-OCR-files/Data/**/"
 echo "KETOS training"
-srun ketos train -f alto  "${XML_FOLDER}/*.xml"
+srun ketos train -f alto --load "${work_directory}/model_5.mlmodel"   "${XML_FOLDER}/*.xml"
